7ea6ff2a01c1d3ef8a43cf4c225d6cc4c390c82d
Quank123Wip <Quank123Wip@outlook.com>
2019-08-27 15:32:49 +0800

    Completed gist

--- lib/gist.py
+++ lib/gist.py
@@ -1,11 +1,21 @@
-from PythonGists.PythonGists import PythonGists
+from PythonGists.PythonGists import PythonGists,Gist
 
-def CreateGist(id:int,content:str,username:str,password:str):
-    if type(id)!=int or type(content)!=str or type(username)!=str or type(password)!=str:
+def CreateGist(uuid:str,content:str,username:str,password:str):
+    if type(uuid)!=str or type(content)!=str or type(username)!=str or type(password)!=str:
         raise TypeError
     try:
         Gist=PythonGists(username=username,password=password)
-        url=Gist.createGist(description='GuOJ Pastebin', content=content, name='GuOJ-Pastebin-'+str(id)+'.md')
+        url=Gist.createGist(description='GuOJ Pastebin', content=content, name='GuOJ-Pastebin-'+uuid+'.md')
     except Exception as error:
         raise error
     return url
+
+def GetGist(url:str):
+    if type(url)!=str:
+        raise TypeError
+    try:
+        gist=Gist(url)
+        Content=gist.getFileContent()
+    except Exception as error:
+        raise error
+    return Content
