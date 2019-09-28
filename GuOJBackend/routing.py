6e8d045cec80dea7d2fb43a4401b846140254670
Quank123Wip <Quank123Wip@outlook.com>
2019-09-28 16:52:54 +0800

    removed useless files

--- GuOJBackend/routing.py
+++ GuOJBackend/routing.py
@@ -1,5 +1,6 @@
 from channels.routing import ProtocolTypeRouter,URLRouter
 from channels.auth import BaseMiddleware,AuthMiddleware
+from django.urls import path,include
 
 application = ProtocolTypeRouter({
     'websocket': AuthMiddleware(
