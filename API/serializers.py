cc9e097d2abadedb5fda9236f7a33bc256353d8b
Quank123Wip <Quank123Wip@outlook.com>
2019-10-23 21:37:00 +0800

    Added Problems Serializers

--- API/serializers.py
+++ API/serializers.py
@@ -1,6 +1,7 @@
 from Databases.models import Problem
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from Databases.models import Problem,ProblemSet
 User = get_user_model()
 
 
@@ -11,4 +12,16 @@ class UserDataSerializers(serializers.ModelSerializer):
         read_only_fields = ['id','username','is_superuser','is_active','Coins','Rating','Experience','Nameplate','NameColor','NameplateColor','UserRegisterDate','Contribution','CompileErrorCount','AcceptedCount','WrongAnswerCount','RuntimeErrorCount','TimeLimitExceededCount','MemoryLimitExceededCount','OutputLimitExceededCount','ParticallyCorrectCount','SystemErrorCount','email']
         depths = 1
 
+class ProblemSetSerializers(serializers.ModuleSerializer):
+    class Meta:
+        model = ProblemSet
+        exclude=[]
+        read_only_fields = []
+        depth = 1
 
+class ProblemSerializers(serializers.ModuleSerializer):
+    class Meta:
+        model = Problem
+        exclude=[]
+        read_only_fields = []
+        depth = 1
