05e012a35d344728c162e5cc81635db963fe5f3f
Quank123Wip <Quank123Wip@outlook.com>
2020-02-03 17:50:15 +0800

    Add deleted tags

--- Databases/models.py
+++ Databases/models.py
@@ -153,6 +153,7 @@ class Discussion(models.Model):
     Like = models.BigIntegerField()
     DisLike = models.BigIntegerField()
     DownVote = models.BigIntegerField()
+    isDeleted = models.BooleanField(default=false)
     class Meta:
       verbose_name = '讨论' 
       verbose_name_plural = verbose_name 
@@ -168,6 +169,7 @@ class Comment(models.Model):
     Like = models.BigIntegerField()
     DisLike = models.BigIntegerField()
     DownVote = models.BigIntegerField()
+    isDeleted = models.BooleanField(default=false)
     class Meta:
       verbose_name = '评论' 
       verbose_name_plural = verbose_name 
