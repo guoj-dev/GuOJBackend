ff7964cced56325fa0889b4eb40fe4c765f356a4
quank123wip <quank123wip@outlook.com>
2020-04-17 06:43:44 +0800

    Rewrite Adapter

--- GuOJBackend/adapter.py
+++ GuOJBackend/adapter.py
@@ -3,10 +3,4 @@ from allauth.account.adapter import DefaultAccountAdapter
 
 
 class CustomAccountAdapter(DefaultAccountAdapter):
-    def get_email_confirmation_url(self, request, emailconfirmation):
-        """Constructs the email confirmation (activation) url.
-        Note that if you have architected your system such that email
-        confirmations are sent outside of the request context `request`
-        can be `None` here.
-        """
-        return emailconfirmation.key
+    pass
